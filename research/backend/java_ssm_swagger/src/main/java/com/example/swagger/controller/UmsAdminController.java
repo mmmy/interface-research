07d76d25381ddf4bc549ae258bb2e2fdca83af1a
yangq06 <yangq06@mingyuanyun.com>
2020-12-27 19:46:28 +0800

    add login

--- research/backend/java_ssm_swagger/src/main/java/com/example/swagger/controller/UmsAdminController.java
+++ research/backend/java_ssm_swagger/src/main/java/com/example/swagger/controller/UmsAdminController.java
@@ -1,13 +1,24 @@
 package com.example.swagger.controller;
 
 import com.example.swagger.common.api.CommonResult;
+import com.example.swagger.dto.LoginResponse;
+import com.example.swagger.dto.UmsAdminLoginParam;
 import com.example.swagger.dto.UmsAdminParam;
 import com.example.swagger.mbg.model.UmsAdmin;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ApiModel
+enum WeekDay {
+    token, tokenHead
+}
 
 @Controller
 @RequestMapping("/admin")
@@ -34,5 +45,19 @@ public class UmsAdminController {
             return CommonResult.failed();
         }
         return CommonResult.success(umsAdmin);
+    }
+
+    @ApiOperation(value = "登录以后返回token")
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult<Map<WeekDay, String>> login(@Validated @RequestBody UmsAdminLoginParam umsAdminLoginParam) {
+        String token = "";//;adminService.login(umsAdminLoginParam.getUsername(), umsAdminLoginParam.getPassword());
+        if (token == null) {
+            return CommonResult.validateFailed("用户名或密码错误");
+        }
+        Map<WeekDay, String> tokenMap = new HashMap<>();
+        tokenMap.put(WeekDay.token, token);
+        tokenMap.put(WeekDay.tokenHead, "todo: tokenHead");
+        return CommonResult.success(tokenMap);
     }
 }
---
+++ research/backend/java_ssm_swagger/src/main/java/com/example/swagger/dto/LoginResponse.java
@@ -0,0 +1,6 @@
+package com.example.swagger.dto;
+
+public class LoginResponse {
+    private String token;
+    private String tokenHead;
+}
