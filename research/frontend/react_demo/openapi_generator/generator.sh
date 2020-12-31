5755904448eb4890b50d6fe6214cecb266c7cd38
yangq06 <yangq06@mingyuanyun.com>
2020-12-31 18:36:54 +0800

    add debug mode

--- research/frontend/react_demo/openapi_generator/generator.sh
+++ research/frontend/react_demo/openapi_generator/generator.sh
@@ -1,3 +1,3 @@
 #npm install @openapitools/openapi-generator-cli -g
 rm tmp -rf
-npx @openapitools/openapi-generator-cli generate -i ../service_codegen/swagger.json -g typescript-node -o ./tmp/test/ -c typescript_axios_config.yaml
+npx @openapitools/openapi-generator-cli generate --global-property debugModels -i ../service_codegen/swagger.json -g typescript-node -o ./tmp/test/ -c typescript_axios_config.yaml
